f91d4674703e0e73d7aff64e18e4d1e49792c93c
ms <orc1226@naver.com>
2017-04-02 16:07:34 +0900

    sangbakddak! - android version in android studio

---
+++ sangbakddak.android/app/src/main/java/kr/ac/hanyang/orc1226/sangbakddak/MainActivity.java
@@ -0,0 +1,32 @@
+package kr.ac.hanyang.orc1226.sangbakddak;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (true == true) {
+                    Snackbar.make(view, "Sangbakddak!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+    }
+}
